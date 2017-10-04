7114d6abc321c51a53946f57b01862b2c5eab784
cdefaria <cdefaria@mail.csuchico.edu>
2017-10-04 03:55:20 +0000

    Broken search

--- main.py
+++ main.py
@@ -18,29 +18,29 @@ class Node:
 nodes=[]
 
 for i in range(1,10):
-    nodes.append(Node(i))
+    nodes.append(Node(str(i)))
 
 g = {'1':{'2':-5, '4':-5}, '2':{'1':5, '3':-2, '5':-3}, '3':{'2':2}, '4':{'1':5}, '5':{'2':3, '6':-8, '8':-2}, '6':{'5':8, '9':-3}, '8':{'5':2, '9':-2},  '9':{'6':3, '8':2}}
 
 def next(c):
-    #print(g[c])
     for key,value in g[c].items():
         if value > 0:
-            if nodes[int(key)].m_checked == 0:
+            if nodes[int(key)-1].m_checked == 0:
                 return key
     return 0
 
 def sumTo(start, end):
     sum=0
-    nodes[int(start)].check()
+    nodes[int(start)-1].check()
     for key,value in g[start].items():
         if value > 0:
             if key == end:
                 return value
-            elif nodes[start].m_checked == 0:
-                sum+=sumTo(key,end)
+            #elif nodes[start-1].m_checked == 0:
             else:
-                return 0
+                sum+=sumTo(key,end)
+            #else:
+            #    return 0
     return sum
 
 def sum_in(id):
@@ -54,14 +54,14 @@ def sum_out(id):
     sum=0
     for key,value in g[id].items():
         if value < 0:
-            sum-=value
+            sum+=abs(value)
     return sum
 
 #filename = input("What file do you want to use?\n")
 endpoint = input("What sensor do you want to be the end point?\n")
 
 
-cur=org=nodes[int(endpoint)]
+cur=nodes[int(endpoint)-1]
 while cur.m_checked==0 and next(str(cur.m_id)):
     if cur.m_id == endpoint:
         cur.m_percent = 1
@@ -72,12 +72,12 @@ while cur.m_checked==0 and next(str(cur.m_id)):
         cur.m_through=sum_out(str(cur.m_id))*cur.m_percent
         cur.m_orig=(sum_out(str(cur.m_id))-sum_in(str(cur.m_id)))*cur.m_through
     cur.check()
-    cur=nodes[int(next(str(cur.m_id)))]
+    cur=nodes[int(next(str(cur.m_id)))-1]
 
 print("Model 1:")
 for i in nodes:
     percent=str(i.m_percent*100)+"% "
-    if(i.m_id%3):
+    if(int(i.m_id)%3):
         print(percent,end='')
     else:
         print(percent)
@@ -85,7 +85,7 @@ for i in nodes:
 print("Model 2:")
 for i in nodes:
     through=str(i.m_through)+" "
-    if(i.m_id%3):
+    if(int(i.m_id)%3):
         print(through,end='')
     else:
         print(through)
@@ -93,7 +93,7 @@ for i in nodes:
 print("Model 3:")
 for i in nodes:
     origin=str(i.m_orig)+" "
-    if(i.m_id%3):
+    if(int(i.m_id)%3):
         print(origin,end='')
     else:
         print(origin)
